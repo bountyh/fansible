070cbd089f209420e5d3c6768bd6d0e72d72ddc4
Pete Ropponen <pete.ropponen@druid.fi>
2016-11-08 18:47:50 +0200

    Added docopt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 extern crate glob;
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
 
 pub mod scanner;
 pub mod initializer;
+
+const USAGE: &'static str = "
+Usage:
+	fansible init <directory> [ --recursive ]
+	fansible <project> [ --list ]
+	fansible ls
+	fansible <project> <play>
+Options:
+  -h --help		Show this screen.
+  -v 			Show version.
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+	arg_directory: String,
+	arg_project: String,
+	arg_play: String,
+	flag_recursive: bool,
+}
 
 fn main() {
 
